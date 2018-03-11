763534779b2477a4d2d0118001998590609455aa
Adrián Velasco <adrivelasco96@hotmail.com>
2018-03-11 18:17:28 -0300

    feat: Redux store and CSS vendor

--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -1,7 +1,3 @@
-/**
- * Create the store with dynamic reducers
- */
-
 import { routerMiddleware } from 'react-router-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
