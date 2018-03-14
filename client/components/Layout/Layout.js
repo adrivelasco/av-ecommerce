88e1365f4efaf871a84a6b70acbc991fb795e0db
adrivelasco <adrivelasco96@hotmail.com>
2018-03-14 18:42:38 -0300

    feat: Menu/navbar improved

--- client/components/Layout/Layout.js
+++ client/components/Layout/Layout.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { Container, Dropdown, Image, Menu } from 'semantic-ui-react';
+import { Container, Button, Menu, Icon } from 'semantic-ui-react';
 
 import s from './Layout.css';
 
-const Layout = ({ children }) => {
+const Layout = ({ children, history, location }) => {
   return (
     <div className={s.root}>
-      <Menu fixed='top' inverted>
+      <Menu fixed="top" inverted>
         <Container>
-          <Menu.Item as='a' header>
-            AV-ECOMMERCE
+          <Menu.Item header>
+            AV eCommerce
           </Menu.Item>
+          <Menu.Item
+            name="home"
+            active={location.pathname === '/'}
+            onClick={() => history.push('/')}
+          />
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Button
+                primary
+                onClick={() => history.push('/mycart')}
+                active={location.pathname === '/mycart'}
+              >
+                <Icon name="shop" />
+                My cart
+              </Button>
+            </Menu.Item>
+          </Menu.Menu>
         </Container>
       </Menu>
       <Container>
