e6f786bbb5aa9b4e8bb60a09e94837d5c2aa944f
Adrián Velasco <adrivelasco96@hotmail.com>
2018-03-15 00:03:11 -0300

    feat: Session middleware and history middleware enabled

--- server/app/index.js
+++ server/app/index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const history = require('connect-history-api-fallback');
 const compression = require('compression');
+const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const sanitized = require('express-sanitized');
 const bodyParser = require('body-parser');
@@ -26,15 +27,31 @@ if (config.env === 'development') {
 
 // Register Node.js middleware
 app.disable('x-powered-by');
-// app.use(history());
 app.use(cookieParser());
 app.use(sanitized());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(compression());
 
+// User session
+app.use(
+  session({
+    name: 'session',
+    secret: '4V3C0MM3RC3',
+    resave: false,
+    saveUninitialized: true,
+    secure: true,
+    cookie: {
+      maxAge: 365 * 24 * 60 * 60 * 1000 * 5
+    }
+  })
+);
+
 // API MarketPlace
 app.use('/api', api);
+
+// Fallback
+app.use(history());
 
 // Routing
 app.use(routes);
