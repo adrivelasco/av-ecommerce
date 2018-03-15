51279ac7f5c4d027b209e53615e325aad216c31a
Adrián Velasco <adrivelasco96@hotmail.com>
2018-03-15 00:01:58 -0300

    feat: Routes for add product and get cart

--- server/api/marketplace/marketplace.controller.js
+++ server/api/marketplace/marketplace.controller.js
@@ -4,12 +4,45 @@ const MarketPlaceModel = require('./marketplace.model');
 
 const MarketPlaceController = {
   getProducts: async (req, res, next) => {
-    console.log('asdsad');
     try {
       const productList = await MarketPlaceModel.getProducts();
       res.json(productList);
-    } catch (e) {
-      next(e);
+    } catch (err) {
+      next(err);
+    }
+  },
+  getCart: (req, res, next) => {
+    try {
+      res.json({
+        statusCode: 200,
+        body: req.session.cart
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+  addProductToCart: (req, res, next) => {
+    try {
+      // Saving
+      console.log(req.session);
+      if (req.session.cart && req.session.cart.length > 0) {
+        let productExists = req.session.cart.find(({ _id }) => req.body._id === _id);
+        if (productExists) {
+          productExists = productExists.quantity + req.body.quantity;
+        } else {
+          req.session.cart.push(req.body);
+        }
+      } else {
+        req.session.cart = [req.body];
+      }
+      res.json({
+        statusCode: '200',
+        body: {
+          success: true
+        }
+      });
+    } catch (err) {
+      next(err);
     }
   }
 };
--- server/api/routes.js
+++ server/api/routes.js
@@ -7,5 +7,7 @@ const router = express.Router();
 
 // MarketPlace Controller
 router.get('/marketplace/products', MarketPlaceController.getProducts);
+router.get('/marketplace/cart', MarketPlaceController.getCart);
+router.post('/marketplace/products/:productId/add-to-cart', MarketPlaceController.addProductToCart);
 
 module.exports = router;
