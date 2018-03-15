582cf7a6f987b8df78b1e8f8e02c8caf16472414
adrivelasco <adrivelasco96@hotmail.com>
2018-03-15 15:04:06 -0300

    feat: Cart endpoint

--- server/api/marketplace/marketplace.controller.js
+++ server/api/marketplace/marketplace.controller.js
@@ -11,6 +11,7 @@ const MarketPlaceController = {
       next(err);
     }
   },
+
   getCart: (req, res, next) => {
     try {
       res.json({
@@ -21,10 +22,30 @@ const MarketPlaceController = {
       next(err);
     }
   },
+
+  removeProductToCart: (req, res, next) => {
+    try {
+      // Delete product from session
+      if (req.session.cart && req.session.cart.length > 0) {
+        // TO-DO: Remove from cart
+        console.log('Remove');
+      } else {
+        req.session.cart = [req.body];
+      }
+      res.json({
+        statusCode: '200',
+        body: {
+          success: true
+        }
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
   addProductToCart: (req, res, next) => {
     try {
-      // Saving
-      console.log(req.session);
+      // Store product on session
       if (req.session.cart && req.session.cart.length > 0) {
         let productExists = req.session.cart.find(({ _id }) => req.body._id === _id);
         if (productExists) {
