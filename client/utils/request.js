37ee9740f4b57720a5f5be14a8c572e614f2d086
Adrián Velasco <adrivelasco96@hotmail.com>
2018-03-15 00:02:27 -0300

    fix: Attach headers and other options

--- client/utils/request.js
+++ client/utils/request.js
@@ -38,6 +38,10 @@ export function checkStatus(response) {
  * @return {object}           The response data
  */
 export default function request(url, options = {}) {
+  options.credentials = 'same-origin';
+  options.mode = 'cors';
+  options.cache = 'default';
+  options.body = JSON.stringify(options.body);
   let headers = new Headers();
   headers.append('Content-Type', 'application/json');
   options.headers = headers;
