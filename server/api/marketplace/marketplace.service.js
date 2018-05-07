50ab739bfa9371e6d739d6cf035e1f5ae5e23fe1
adrivelasco <adrivelasco96@hotmail.com>
2018-05-07 12:13:09 -0300

    feat: requestPromise renamed to rp

--- server/api/marketplace/marketplace.service.js
+++ server/api/marketplace/marketplace.service.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const requestPromise = require('request-promise');
+const rp = require('request-promise');
 const config = require('../../config');
 
 const MarketPlaceService = {
@@ -10,14 +10,13 @@ const MarketPlaceService = {
    * @description return data of all products
    */
   getProducts: async () => {
-    const options = {
-      method: 'GET',
-      uri: config.apiMarketPlace,
-      json: true,
-      resolveWithFullResponse: true
-    };
     try {
-      const response = await requestPromise(options);
+      const response = await rp({
+        method: 'GET',
+        uri: config.apiMarketPlace,
+        json: true,
+        resolveWithFullResponse: true
+      });
       return Promise.resolve(response);
     } catch (error) {
       return Promise.reject(error);
